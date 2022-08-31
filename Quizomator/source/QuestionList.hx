d71d2be6c904c1983c6c2b9cadf221ec27be9fb2
KolczatkowyToNieJestPrzymiotnik <84466597+teotm@users.noreply.github.com>
2022-08-31 17:01:05 +0200

    smthn

---
+++ Quizomator/source/QuestionList.hx
@@ -0,0 +1,8 @@
+package;
+
+import SingularQuestion.TheFunnyQuestion;
+
+typedef LeQuestionList =
+{
+	var questions:Array<TheFunnyQuestion>;
+}
---
+++ Quizomator/source/SingularQuestion.hx
@@ -0,0 +1,11 @@
+package;
+
+typedef TheFunnyQuestion =
+{
+	var number:Int;
+	var question:String;
+	var correctAnswerList:Null<Array<String>>;
+	var punishment:Bool;
+	var prize:Bool;
+	var answers:Null<Array<String>>;
+}
